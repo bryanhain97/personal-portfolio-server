392a4bf2d71aeda055eec7bb2d4b9544d9137c0c
Bryan Hain <bryanhain97@gmail.com>
2022-02-27 23:09:53 +0100

    use cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cool = require('cool-ascii-faces')
 
 app.use(express.json())
+app.use(cors())
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>  {
     console.log(`App listening on PORT: ${PORT}`)
