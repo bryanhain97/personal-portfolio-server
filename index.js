bea28d87d75e0996a3e02d0e1c05b06b5f0db7af
Bryan Hain <bryanhain97@gmail.com>
2022-02-28 22:07:08 +0100

    install stripe

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const express = require('express');
 const app = express();
 const cool = require('cool-ascii-faces')
 const cors =  require('cors');
-
+const stripe = require('stripe')('sk_test_51KWQn4BU58LFQRGJ6ChYO0mTdacnp7JtW2CUVDxt9Z4IuIDzXPHIjtVkJQw6niQZLFkbXp9lSSTm16FYeeHCpPWa00QeSNimEg')
 app.use(express.json())
 app.use(cors())
 const PORT = process.env.PORT || 5000
+
+const YOUR_DOMAIN = "https://www.thatguybryan.com"
+const customer = await stripe.customer.create({
+    description: 'My first Stripe customer'
+})
+const paymentIntent = await stripe.paymentIntent.create({
+    customer: '42424', // customer id
+    currency: 'eur',
+    amount: 250,
+    payment_method_types: ['card'],
+    setup_future_usage: 'on_session'
+})
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        line_items: [{
+            price: 250,
+            quantity: 1,
+        }],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}?success=true`,
+        cancel_url: `${YOUR_DOMAIN}?canceled=true`
+    })
+    res.redirect(303, session.url)
+})
+
+
 app.listen(PORT, () =>  {
     console.log(`App listening on PORT: ${PORT}`)
 })
