807335dc129a8f17f3beaa26c12dff0c6e91c5b9
Bryan Hain <bryanhain97@gmail.com>
2022-03-05 15:46:40 +0100

    checkout session coffee

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const cool = require('cool-ascii-faces')
 const cors = require('cors');
 require('dotenv').config();
 
+
 app.use(express.json())
 app.use(cors())
-const stripe = require("stripe")(process.env.STRIPE_TESTPRIVATEKEY);
+const stripe = require("stripe")(process.env.SERVER_TESTPRIVATEKEY);
 const items = new Map([
-    [1, { priceInCents: 250, name: 'coffee' }]
+    [1, { priceInCents: 250, name: 'A cup of coffee for Bryan' }]
 ])
 const PORT = process.env.PORT || 1220
 
@@ -23,7 +24,8 @@ app.post('/create-checkout-session', async (req, res) => {
                         price_data: {
                             currency: "eur",
                             product_data: {
-                                name: item.name
+                                name: item.name,
+                                images: ["https://upload.wikimedia.org/wikipedia/commons/0/09/1-1267462282CoNq.jpg"]
                             },
                             unit_amount: item.priceInCents,
                         },
@@ -31,9 +33,10 @@ app.post('/create-checkout-session', async (req, res) => {
                     }
                 }),
                 mode: "payment",
-                redirect_url: "https://thatguybryan.com"
+                success_url: "https://thatguybryan.com",
+                cancel_url: "https://thatguybryan.com"
             })
-        res.json(session.redirect_url)
+            res.json(session.url)
     } catch (e) {
         res.status(500).json({ error: e.message })
     }
